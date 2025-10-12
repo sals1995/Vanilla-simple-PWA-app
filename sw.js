c8f4381ed7309056d603ff08ea6c7a7c5b35c911
Salsabeel <58127260+sals1995@users.noreply.github.com>
2025-10-12 12:54:48 +0300

    updating index.html and manifest icons

--- sw.js
+++ sw.js
@@ -1,30 +1,42 @@
+const paths=[
+    "/",
+    "/index.html",
+    "/js/main.js",
+    "/js/other.js",
+    "/styles/index.css",
+    "/styles/other.css",
+    "/other.html"    
+]
+
 self.addEventListener("install",(event)=>{
-    console.log("installed !!!!!");
+    console.log("installed");
     self.skipWaiting()
     event.waitUntil(
-        caches.open("our-app").then((cache)=> cache.addAll(["/","index.html","styles/index.css","js/main.js","other.html","styles/other.css","js/other.js"]))
+        caches.open("our-site").then((cache)=>{
+           return cache.addAll(paths)
+        })
     )
 })
-
 self.addEventListener("activate",()=>{
-    console.log("activated");
+    console.log("activated !!!!!!");
     
 })
 
-
 self.addEventListener("fetch",(event)=>{
-    console.log("send req to ",event.request.url);
-    event.respondWith(
-        // cache first
+    console.log("sending request to",event.request.url);
+    event.respondWith( 
         caches.match(event.request.url).then((file)=>{
             if(file){
                 console.log("file found in cache");
                 return file
             }
             else{
-                console.log("file Not found in cache");
-               return fetch(event.request.url)
+                console.log("file NOT found in cache");
+                return fetch(event.request.url)
             }
         })
+        //or 
+        // caches.match(event.request.url).then((file)=> file || fetch(event.request.url))
+        //or you can use async + await instead of .then() + .catch()
     )
 })
