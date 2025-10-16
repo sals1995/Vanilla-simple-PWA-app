6695251f99d4ed2037de16c3d2569b8203e8c25b
Salsabeel <58127260+sals1995@users.noreply.github.com>
2025-10-16 12:55:05 +0300

    Adding caches login and manifest file

--- sw.js
+++ sw.js
@@ -1,42 +1,46 @@
-const paths=[
+const cacheName="our-PWA"
+const cachesFiles=[
     "/",
     "/index.html",
     "/js/main.js",
-    "/js/other.js",
     "/styles/index.css",
+    "/js/other.js",
     "/styles/other.css",
-    "/other.html"    
-]
+    "/other.html",
 
+]
 self.addEventListener("install",(event)=>{
     console.log("installed");
     self.skipWaiting()
     event.waitUntil(
-        caches.open("our-site").then((cache)=>{
-           return cache.addAll(paths)
+        caches.open(cacheName).then(cache=>{
+            cache.addAll(cachesFiles)
         })
     )
 })
 self.addEventListener("activate",()=>{
-    console.log("activated !!!!!!");
+    console.log("activated");
     
 })
 
 self.addEventListener("fetch",(event)=>{
-    console.log("sending request to",event.request.url);
-    event.respondWith( 
-        caches.match(event.request.url).then((file)=>{
-            if(file){
-                console.log("file found in cache");
-                return file
-            }
-            else{
-                console.log("file NOT found in cache");
-                return fetch(event.request.url)
-            }
-        })
-        //or 
-        // caches.match(event.request.url).then((file)=> file || fetch(event.request.url))
+    console.log( "request send to:", event.request.url);
+    event.respondWith(
+        caches.match(event.request.url).then(res=> res || fetch(event.request.url))
+        //or
+    //     caches.match(event.request.url).then(res=>
+    //         {
+    //         if (res) {
+    //             console.log("Found in cache");
+    //             return res
+    //         }
+    //         else{
+    //             console.log("Not Found in cache");
+    //            return fetch(event.request.url)
+    //         }
+    //     }
+    // )
         //or you can use async + await instead of .then() + .catch()
     )
 })
+
